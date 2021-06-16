bcbe8d5eea628a6d5b78db0f4dd0aa8471b7ffca
arun <arunberry47@gmail.com>
2021-06-17 00:11:34 +0530

    adding stuff

---
+++ dist/_nuxt/static/1623868733/thanks/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623868733",layout:"main",error:a,state:{authenticated:a,user:{}},serverRendered:true,routePath:"\u002Fthanks",config:{googleAnalytics:{id:"G-NX2J2T0C5E"},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
